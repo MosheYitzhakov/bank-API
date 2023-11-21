afa04ccafff02ca4968331d4789a8edc50beac6f
shimon riss <shimy1045@gmail.com>
2023-11-21 17:59:03 +0200

    Termination of DB without normality checks and restrictions

--- dbUsers.js
+++ dbUsers.js
@@ -23,55 +23,104 @@ const addUser = async (name, passport) => {
     VALUE();
 `;
     let [{ insertId }] = await pool.query(sql);
-
-console.log(insertId);
     sql = `
     INSERT INTO users (name, passport, id_account)
     VALUES (?, ?,?)
 `;
     [{ insertId }] = await pool.query(sql, [name, passport, insertId]);
-console.log(insertId);
-    return getUser(insertId);
+    return getUser(passport);
 }
 
-const depositing = () => {    // הפקדה
-
+const depositing = async (passport, cash) => {
+    const { id_account } = await getUser(passport);
+    const sql = `
+        UPDATE accounts
+        SET cash = ?
+        WHERE id_account = ?
+    `;
+    const [{ affectedRows }] = await pool.query(sql, [cash, id_account]);
+    if (affectedRows) return getUser(passport);
 }
 
-const updateCredit = () => {
-
+const updateCredit = async (passport, credit) => {
+    const { id_account } = await getUser(passport);
+    const sql = `
+        UPDATE accounts
+        SET credit = ?
+        WHERE id_account = ?
+    `;
+    const [{ affectedRows }] = await pool.query(sql, [credit, id_account]);
+    if (affectedRows) return getUser(passport);
 }
 
-const withdrawMoney = () => { // משיכת כספים
-
+const withdrawMoney = async (passport, cash) => { // משיכת כספים
+    const { id_account } = await getUser(passport);
+    const sql = `
+            UPDATE accounts
+            SET cash = cash - ?
+            WHERE id_account = ?
+        `;
+    const [{ affectedRows }] = await pool.query(sql, [cash, id_account]);
+    if (affectedRows) return getUser(passport);
 }
-const transferring = () => {  //  ע"י אשראי עברת כספים לחשבון אחר
-
+const transferring = async (passport, numMoney, ohterName) => {  //  ע"י אשראי עברת כספים לחשבון אחר
+    let { id_account } = await getUser(passport);
+    let otherAccount = await getUser(await getIdUserForPassport(ohterName));
+    const sql2 = `
+    UPDATE accounts
+    SET credit = credit - ?
+    WHERE id_account = ?
+    `
+    const sql = `
+    UPDATE accounts
+    SET cash = cash + ?
+    WHERE id_account = ?;
+    `;
+    let [{ affectedRows }] = await pool.query(sql2, [numMoney, id_account]);
+    if (affectedRows) {
+        [{ affectedRows }] = await pool.query(sql, [numMoney, otherAccount.id_account]);
+        if (affectedRows) return console.log('transferring');
+    }
 }
-const getUser = async (id) => {
+const getUser = async (passport) => {
     const sql = `
     SELECT *
     FROM users
-    WHERE id_user = ?;
+    JOIN accounts
+    USING(id_account)
+    WHERE passport  = ?;
 `;
-    const [[res]] = await pool.query(sql, [id]);
-    console.log(res);
+    const [[res]] = await pool.query(sql, [passport]);
+    console.table(res);
     return res;
 }
 
-const getIdUserForName = async (name) => {
+const getIdUserForPassport = async (name) => {
     const sql = `
-    SELECT id_user
+    SELECT passport
     FROM users
     WHERE name = ?;
 `;
     const [[res]] = await pool.query(sql, [name]);
-    return res;
+    console.log(res.passport);
+    return res.passport;
 }
-const getAllUsers = () => {
+const getAllUsers = async () => {
+    const sql = `
+SELECT *
+FROM users
+    JOIN accounts
+    USING(id_account) `
+    const [res] = await pool.query(sql)
+    console.table(res);
+    return res
+}
+const tast = async () => {
+    await depositing(123123123, 1250)
+    getAllUsers()
+}
+tast()
 
-}
-addUser('shimy',123123123)
 
 
 
